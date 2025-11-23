d896b10cf09e7edea11ab65f5bd266908fcd3ecf
Sas-Kirakosyan <sasun-kirakosyan@mail.ru>
2025-11-23 18:38:35 +0400

    add datepicker

---
+++ src/components/DatePicker/DatePicker.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+
+// use DatePicker via conditional rendering isOpenDatePicker && DayPicker
+export const DatePicker = () => {
+  const [selected, setSelected] = useState<Date>();
+
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={
+        selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
+      }
+    />
+  );
+};
