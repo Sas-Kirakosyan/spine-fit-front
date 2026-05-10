54351237100cfcea9d4c518855a8778310676095
Rafael Grigoryan <rafogrig2003@mail.ru>
2026-05-10 21:40:52 +0400

    active symptoms lift up

--- packages/shared/src/quiz/constants.ts
+++ packages/shared/src/quiz/constants.ts
@@ -9,13 +9,13 @@ export const GOAL_HYPERTROPHY: GoalOption = GOAL_OPTIONS[0];
 export const GOAL_RECOVERY: GoalOption = GOAL_OPTIONS[1];
 
 export const PAIN_STATUS_QUIZ_OPTIONS = [
-  "Healthy (I am pain-free, but cautious)",
-  "Recovered (Past history of pain/injury)",
   "Active Symptoms (Currently experiencing discomfort)",
+  "Recovered (Past history of pain/injury)",
+  "Healthy (I am pain-free, but cautious)",
 ] as const;
 
 export type PainStatusQuizOption = (typeof PAIN_STATUS_QUIZ_OPTIONS)[number];
 
-export const PAIN_STATUS_HEALTHY: PainStatusQuizOption = PAIN_STATUS_QUIZ_OPTIONS[0];
+export const PAIN_STATUS_ACTIVE: PainStatusQuizOption = PAIN_STATUS_QUIZ_OPTIONS[0];
 export const PAIN_STATUS_RECOVERED: PainStatusQuizOption = PAIN_STATUS_QUIZ_OPTIONS[1];
-export const PAIN_STATUS_ACTIVE: PainStatusQuizOption = PAIN_STATUS_QUIZ_OPTIONS[2];
+export const PAIN_STATUS_HEALTHY: PainStatusQuizOption = PAIN_STATUS_QUIZ_OPTIONS[2];
