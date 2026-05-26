cf773a1d06bee34cc7c323d46b4e88aca2f179f2
Samvel Grigoryan <samvelgrig666@gmail.com>
2026-05-26 14:45:08 +0400

    feat: add clear icon for input

--- apps/web/src/components/Icons/Icons.tsx
+++ apps/web/src/components/Icons/Icons.tsx
@@ -317,3 +317,23 @@ export function HomeIcon({
     </button>
   );
 }
+
+
+export function ClearIcon({ className = "h-5 w-5", onClickClearIcon }: { className?: string, onClickClearIcon?: () => void }) {
+    return (
+        <svg
+            onClick={onClickClearIcon}
+            aria-hidden="true"
+            className={className}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+        </svg>
+    );
+}
